06eba4789a720fc40804c1a9ddc4f41ec7b6c671
Akash Joshi <akash.joshi@viit.ac.in>
2021-09-26 10:14:48 +0530

    feat: display avg rating

--- frontjs/src/index.ts
+++ frontjs/src/index.ts
@@ -1,10 +1,23 @@
 console.log("hello world");
 
 const initAverageRating = (avgRating) => {
+  const starRating = Math.ceil(avgRating);
+
+  document.querySelector(".avg-rating").innerHTML = avgRating;
+  document.querySelector(".avg-rating-stars").setAttribute("title", avgRating);
+
+  const stars = document.querySelectorAll(".avg-rating-stars svg");
+
+  for (let index = 0; index < starRating; index++) {
+    const star = stars[index];
+
+    star.classList.add("active");
+  }
+
   document.querySelector(".main-content").classList.remove("display-none");
   document.querySelector(".loading").classList.add("display-none");
 };
 
-initAverageRating(3.8);
+initAverageRating(2.8);
 
 // fill #FDCE71
