7fa45621bf631748bb54ac445bfa3514786adc45
Akash Joshi <akash.joshi@viit.ac.in>
2021-09-26 10:02:42 +0530

    refactor: make svgs fillable

--- frontjs/src/index.ts
+++ frontjs/src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello world")
+console.log("hello world");
+
+const initAverageRating = (avgRating) => {
+  document.querySelector(".main-content").classList.remove("display-none");
+  document.querySelector(".loading").classList.add("display-none");
+};
+
+initAverageRating(3.8);
+
+// fill #FDCE71
